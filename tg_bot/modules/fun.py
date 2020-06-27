3a64671212ded36a2cfd0d8f6f46d842068951c2
silencespeaks7 <63138395+silencespeaks7@users.noreply.github.com>
2020-06-27 12:36:55 +0530

    Update fun.py

--- tg_bot/modules/fun.py
+++ tg_bot/modules/fun.py
@@ -26,7 +26,7 @@ from tg_bot.modules.disable import DisableAbleCommandHandler
 from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.extraction import extract_user
 
-import nltk
+
 
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
